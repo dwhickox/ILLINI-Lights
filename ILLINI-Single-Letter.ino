1e988576848334e117318dd40df94bcd2a8a7900
David Hickox <dwhickox@users.noreply.github.com>
2018-02-19 20:50:03 -0600

    Update ILLINI-Single-Letter.ino

--- ILLINI-Single-Letter.ino
+++ ILLINI-Single-Letter.ino
@@ -8,15 +8,12 @@
 int pixelrand;
 int colorrand;
 // Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
+
 #define PIN            0 
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      50
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_BRG + NEO_KHZ800);
 
 int delayval = 5000; // delay for half a second
